dcfbceaf86533a052591b3187db83a5e197e585c
TopeAanu <topefasasi99@gmail.com>
2025-04-08 22:46:06 +0100

    setup the metadata

--- app/layout.js
+++ app/layout.js
@@ -14,10 +14,32 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "School Information System",
-  description: "The official webpage of SIS. Login to access your profile!",
+  title: "SchoolMSy",
+  description: "A School Management System web app",
   icons: {
-    icon: "/favicon32x32.png",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: {
+      url: "/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
   },
 };
 
