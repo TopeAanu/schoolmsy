cd4c41b6936e3fff1d6712c34ac12a84caeabd53
TopeAanu <topefasasi99@gmail.com>
2025-04-12 23:29:45 +0100

    updated metadata

--- app/layout.js
+++ app/layout.js
@@ -14,9 +14,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "SchoolMsy",
+  title: "SchoolMsy | A School Management System App for creating, maintaining and managing students data",
   description:
-    "SchoolMsy is a School Management System App for creating, maintaining and managing students data",
+    "School-msy is a School Management System. It does the following: create student profile, authenticate admin, generate username and password for student, assign assignment to student, grade student's assignment, verification before adding new admin, edit student data.",
   icons: {
     icon: [
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
